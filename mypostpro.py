3378e01895338c9bd4e890ceb0901844240acbf6
Sean <r04246004@ntu.edu.tw>
2020-03-29 17:22:30 -0500

    add functionality

--- mypostpro.py
+++ mypostpro.py
@@ -1,4 +1,30 @@
+
+
 def cnuss_err(fmean,fvar,rmean,rvar):
     mean_err = abs(fmean-rmean)/abs(fmean)
     var_err = abs(rvar-fvar)/abs(fvar)
     return mean_err, var_err
+
+
+def cmean(data,idx):
+    import numpy as np
+    mean = np.sum(data[:,idx])/len(data[:,idx])
+    return mean
+
+
+
+def cvar(data,mean,idx):
+    import numpy as np
+    var = np.sum((data[:,idx]-mean)**2)/(len(data[:,idx])-1)
+    return var 
+
+
+def read_nuss(fname):
+    import numpy as np
+    with open(fname, 'r') as f:
+        k = f.read()
+    list_of_lines = k.split('\n')
+    list_of_words = [[k for k in line.split(' ') if k and 
+        k != 'nuss' and k != 'nus'] for line in list_of_lines][:-1]
+    nuss = np.array(list_of_words).astype(np.float64)
+    return nuss
