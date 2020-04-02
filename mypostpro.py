1031a64202c9c2b88d8bf504b9b8a37602445279
Sean <r04246004@ntu.edu.tw>
2020-04-02 15:01:24 -0500

    add computation for standard deviation

--- mypostpro.py
+++ mypostpro.py
@@ -1,9 +1,10 @@
 
 
-def cnuss_err(fmean,fvar,rmean,rvar):
+def cnuss_err(fmean,fvar,fsd,rmean,rvar,rsd):
     mean_err = abs(fmean-rmean)/abs(fmean)
     var_err = abs(rvar-fvar)/abs(fvar)
-    return mean_err, var_err
+    sd_err = abs(rsd-fsd)/abs(fsd)
+    return mean_err, var_err, sd_err
 
 
 def cmean(data,idx):
@@ -11,12 +12,15 @@ def cmean(data,idx):
     mean = np.sum(data[:,idx])/len(data[:,idx])
     return mean
 
-
-
 def cvar(data,mean,idx):
     import numpy as np
     var = np.sum((data[:,idx]-mean)**2)/(len(data[:,idx])-1)
     return var 
+
+def csd(data,mean,idx):
+    import numpy as np
+    sigma = np.sqrt(cvar(data,mean,idx))
+    return sigma
 
 
 def read_nuss(fname):
