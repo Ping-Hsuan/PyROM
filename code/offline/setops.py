76e2aa999783b43194533787ad8a473854c465b5
Sean <r04246004@ntu.edu.tw>
2020-08-10 15:39:29 -0500

    add setops.py

---
+++ code/offline/setops.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def setops(N):
+    a = seta('./au',N)
+    b = setb('./bu',N)
+    c = setc('./cu',N)
+
+    return a, b, c
+
+def seta(fname,N):
+    t = np.loadtxt(fname)
+    lb = int(np.sqrt(len(t)))
+    t = np.reshape(t, (lb, lb))
+    a = t[1:N+1, 1:N+1]
+    return a
+
+
+def setb(fname,N):
+    t = np.loadtxt(fname)
+    lb = int(np.sqrt(len(t)))
+    t = np.reshape(t, (lb, lb))
+    b = t[1:N+1, 1:N+1]
+    return b
+
+
+def setc(fname,N):
+    t = np.loadtxt(fname)
+    lb = int(np.floor((len(t))**(1/3)))
+    t = np.reshape(t, (lb, lb+1, lb+1))
+    c = t[0:N, 0:N+1, 0:N+1]
+    return c
