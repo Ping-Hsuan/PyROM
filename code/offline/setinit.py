b2cb0d2d5865fa41933aa7e962304cc67eefeea1
Sean <r04246004@ntu.edu.tw>
2020-08-10 19:08:27 -0500

    add support for qoi and init

---
+++ code/offline/setinit.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('../')
+from read_helpers.read_helpers import *
+
+def setinit(fname, N):
+    return read_vector(fname)
---
+++ code/offline/setqoi.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('../')
+from read_helpers.read_helpers import *
+
+def setqoi(fname, N):
+    return read_vector(fname)
