ac8cb5085cd852e590b45f6b29cd42663379d39a
Sean <r04246004@ntu.edu.tw>
2020-08-11 15:42:47 -0500

    use class for setting operator

--- code/offline/setops_new.py
+++ code/offline/setops_new.py
@@ -5,11 +5,33 @@ from read_helpers.read_helpers import *
 
 class setops_u:
     def __init__(self, dir_a, dir_b, dir_c, size):
-        self.a = self.seta(dir_a, size)
+        self.a = read_mat(dir_a, size)
+        self.b = read_mat(dir_b, size)
+        self.c = read_tensor(dir_c, size)
         self.size = size
-    def seta(self, fname, N):
-        t = read_mat(fname, N)
-        a = t[1:N+1, 1:N+1]
-        return a
 
+class setops_new:
+    def __init__(self, *arg, size):
+        if len(arg) > 3:
+            if 'but_x' in arg[3].split('/'):
+                print('correct operator directory name')
+                self.but_x = read_mat(arg[3], size)
+            else: 
+                print('operator directory name in argument '+str(3)+\
+                        'is incorrect')
+            if 'but_y' in arg[4].split('/'):
+                print('correct operator directory name')
+                self.but_y = read_mat(arg[4], size)
+            else: 
+                print('operator directory name in argument '+str(4)+ \
+                        'is incorrect')
+            self.a = read_mat(arg[0], size)
+            self.b = read_mat(arg[1], size)
+            self.c = read_tensor(arg[2], size)
+            self.size = size
+        else:
+            self.a = read_mat(arg[0], size)
+            self.b = read_mat(arg[1], size)
+            self.c = read_tensor(arg[2], size)
+            self.size = size
 
