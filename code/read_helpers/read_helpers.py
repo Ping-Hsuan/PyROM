381cb1627cb8311a5b05131afc5af769e57de9c6
Sean <r04246004@ntu.edu.tw>
2020-08-11 16:05:39 -0500

    update read helpers

--- code/read_helpers/read_helpers.py
+++ code/read_helpers/read_helpers.py
@@ -4,12 +4,19 @@ import numpy as np
 def read_mat(fname, N):
     t = np.loadtxt(fname)
     lb = int(np.sqrt(len(t)))
-    t = np.reshape(t, (lb, lb))
+    t1 = np.reshape(t, (lb, lb))
+    t = t1[1:N+1, 1:N+1]
     return t
 
 
 def read_tensor(fname, N):
     t = np.loadtxt(fname)
     lb = int(np.floor((len(t))**(1/3)))
-    t = np.reshape(t, (lb, lb+1, lb+1))
+    t1 = np.reshape(t, (lb, lb+1, lb+1))
+    t = t1[0:N, 0:N+1, 0:N+1]
+    return t
+
+
+def read_vector(fname):
+    t = np.loadtxt(fname)
     return t
