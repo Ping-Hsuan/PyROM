6874abcd3893ae5eb629dc3e8debc555bdfbfc58
Sean <r04246004@ntu.edu.tw>
2020-08-12 16:13:12 -0500

    read data in fortran order

--- code/read_helpers/read_helpers.py
+++ code/read_helpers/read_helpers.py
@@ -4,15 +4,19 @@ import numpy as np
 def read_mat(fname, N):
     t = np.loadtxt(fname)
     lb = int(np.sqrt(len(t)))
-    t1 = np.reshape(t, (lb, lb))
-    t = t1[1:N+1, 1:N+1]
+    t1 = np.reshape(t, (lb, lb), order='F')
+    msg = fname.split('/')
+    if 'but' in msg[-1].split('_'):
+        t = t1[0:N+1, 0:N+1]
+    else:
+        t = t1[0:N+1, 0:N+1]
     return t
 
 
 def read_tensor(fname, N):
     t = np.loadtxt(fname)
     lb = int(np.floor((len(t))**(1/3)))
-    t1 = np.reshape(t, (lb, lb+1, lb+1))
+    t1 = np.reshape(t, (lb, lb+1, lb+1), order='F')
     t = t1[0:N, 0:N+1, 0:N+1]
     return t
 
