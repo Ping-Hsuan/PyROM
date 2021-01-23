e355c8527748427a82e5103a5170ede33c2e96b1
Sean <r04246004@ntu.edu.tw>
2021-01-23 16:24:37 -0600

    update dual_norm.py

--- postprocessing/dual_norm.py
+++ postprocessing/dual_norm.py
@@ -28,12 +28,23 @@ print("Argument List:", str(sys.argv))
 print(os.getcwd())
 os.chdir(str(sys.argv[1]))
 print(os.getcwd())
-os.chdir('./crom/')
+isExist = os.path.exists(os.getcwd()+'/dual_norm/')
+if isExist:
+    pass
+else:
+    os.mkdir(os.getcwd()+'/dual_norm/')
 print(os.getcwd())
+#os.chdir('./crom/')
+#print(os.getcwd())
 
-p1 = subprocess.Popen('ls'.split(), stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-filenames = (p1.stdout.read()).decode().split('\n')[:-1]
-p1.wait()
+for root, dirs, files in os.walk("./crom/", topdown=False):
+    for name in files:
+        if re.match('^.*_(.*)rom_.*$', name):
+            print(os.path.join(root, name))
+    for name in dirs:
+        print(os.path.join(root, name))
+
+filenames = [name for name in files if re.match('^.*_(.*)rom_.*$', name)]
 
 dic_for_files = {}
 
@@ -43,8 +54,6 @@ for fname in filenames:
         if int(match_nb.groups()[0]) not in dic_for_files:
             dic_for_files[int(match_nb.groups()[0])] = []
         dic_for_files[int(match_nb.groups()[0])].append(fname)
-
-print(dic_for_files)
 
 colors = cm.Set1(np.linspace(0, 1, 9))
 color_ctr = 0
@@ -62,6 +71,7 @@ dict_final = sorted(dic_for_files.items(), key=operator.itemgetter(0))
 
 i = 0
 pattern = re.compile(r'(residual in h1 norm:\s\s+)(\d\.\d+E?-?\d+)')
+tpath = './dual_norm/'
 fig, ax = plt.subplots(1, tight_layout=True)
 for nb, fnames in dict_final:
     erri = []
@@ -79,15 +89,22 @@ for nb, fnames in dict_final:
             solver = 'Constrained ROM'
         elif match_rom.groups()[0] == 'l':
             solver = 'Leray ROM'
+
         assert match_rom is not None
-        with open(fname, 'r') as f:
+
+        with open(root+fname, 'r') as f:
             contents = f.read()
             matches = pattern.finditer(contents)
             for match in matches:
                 print(match)
                 dual_norm = match.group(2)
+
         erri.append(float(dual_norm))
         angle.append(int(forleg[-1])+90)
+
+        with open(tpath+fname+'_dn', 'w') as f:
+            f.write(dual_norm)
+
     data = np.column_stack((angle, erri))
     data = data[data[:, 0].argsort()]
     print(data[:, 0])
@@ -95,7 +112,7 @@ for nb, fnames in dict_final:
     print(type(data[:, 0]))
     print(type(data[:, 1]))
     ax.semilogy(data[:, 0], data[:, 1], '-o', color=colors[i],
-                  mfc="None", label=r'$N = $'+str(nb))
+                mfc="None", label=r'$N = $'+str(nb))
     i += 1
 
 ax.set_ylabel(r'$\triangle$')
@@ -103,6 +120,6 @@ ax.set_xlabel(r'$\theta_g$')
 ax.set_xticks(np.linspace(0, 180, 5, dtype=int))
 ax.set_ylim([1e-2, 1])
 ax.legend(loc=0, ncol=2)
-fig.savefig('online.png')
-np.savetxt('angle.dat', data[:, 0])
-np.savetxt('erri.dat', data[:, 1])
+fig.savefig(tpath+'online.png')
+np.savetxt(tpath+'angle.dat', data[:, 0])
+np.savetxt(tpath+'erri.dat', data[:, 1])
