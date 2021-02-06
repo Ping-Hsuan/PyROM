5a7b9b26b96fe73044f5f67fd80906f0b637945c
Sean <r04246004@ntu.edu.tw>
2021-02-06 11:07:20 -0600

    update romt.py

--- postprocessing/romt.py
+++ postprocessing/romt.py
@@ -3,12 +3,8 @@ import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from matplotlib.ticker import MaxNLocator
-from itertools import accumulate
-from matplotlib.ticker import ScalarFormatter, NullFormatter
 import re
 import os
-import subprocess
-import operator
 import sys
 sys.path.append('/Users/bigticket0501/Developer/PyMOR/code/plot_helpers/')
 import mypostpro
@@ -26,10 +22,9 @@ matplotlib.rcParams['text.latex.preamble'] = [
 print("---------------------------------------------")
 print("This is the name of the program:", sys.argv[0])
 print("Argument List:", str(sys.argv))
-print(os.getcwd())
 os.chdir(str(sys.argv[1]))
-print(os.getcwd())
 print("---------------------------------------------")
+N = str((sys.argv[2]))
 isExist = os.path.exists(os.getcwd()+'/romt/')
 if isExist:
     print("The target directory exist")
@@ -38,14 +33,16 @@ else:
     os.mkdir(os.getcwd()+'/romt/')
     print("The target directory exist")
 print("---------------------------------------------")
+ops_path = '../ops/'
 
 root = os.getcwd()
 sp1 = (root.split('/'))
-sp2 = (sp1.pop())
-sp3 = sp2.split('_')
-anchor = float(sp3.pop())
+for element in sp1:
+    z = re.match(r"theta_(\d+)", element)
+    if z:
+        anchor = float(((z.groups())[0]))
 
-for root, dirs, files in os.walk("./rom/", topdown=False):
+for root, dirs, files in os.walk("./romt/", topdown=False):
     for name in files:
         if re.match('^.*_(.*)rom_.*$', name):
             pass
@@ -54,7 +51,7 @@ for root, dirs, files in os.walk("./rom/", topdown=False):
         pass
 #       print(os.path.join(root, name))
 
-fname = [name for name in files if re.match('^.*_(.*)rom_.*_'+str(int(anchor-90)), name)]
+filenames = [name for name in files if re.match('^.*_(.*)rom_.*_'+str(int(anchor-90)), name)]
 
 colors = cm.Set1(np.linspace(0, 1, 9))
 color_ctr = 0
@@ -64,7 +61,10 @@ tpath = './romt/'
 romt = []
 t_grid = []
 
-fname = fname.pop()
+for element in filenames:
+    z = re.match(r"^.*_(\d+)nb_.*", element)
+    if z.groups()[0] == N:
+        fname = element
 match_rom = re.match('^.*_(.*)rom_.*$', fname)
 assert match_rom is not None
 
@@ -75,59 +75,79 @@ elif match_rom.groups()[0] == 'c':
 elif match_rom.groups()[0] == 'l':
     solver = 'Leray ROM'
 
-ft = open(tpath+fname+'_romt', 'w')
-with open(root+fname, 'r') as f:
+with open(tpath+fname, 'r') as f:
     for line in f:
-        if re.search(r'\sromt', line):
-            ft.write(line)
-            info = line.split()
-            romt.append(info[2])
-            t_grid.append(info[1])
-ft.close()
+        info = line.split()
+        romt.append(info[2])
+        t_grid.append(info[1])
 
 K_match = re.match(r"(\d\d\d)s_.*", fname)
 nb_match = re.match(r"^.*_(\d+)nb_.*", fname)
 K = int(K_match.groups()[0])
 nb = int(nb_match.groups()[0])
 T = int(len(romt)/nb)
+sp1 = fname.split('_')
+for element in sp1:
+    if re.match(r"zero", element):
+        T0 = K
+    elif re.match(r"ic", element):
+        T0 = 0
+        t_grid = np.reshape(np.array(t_grid).astype(np.float64),
+                            (nb, T), order='F')
+        t_grid += 500
+
+print(f'Information K: {K}, N: {nb}, T:{T}, T0: {T0}')
 romt = np.reshape(np.array(romt).astype(np.float64),
                   (nb, T), order='F')
-t_grid = np.reshape(np.array(t_grid).astype(np.float64),
-                    (nb, T), order='F')
-fomt = np.loadtxt('tk')
+fomt = np.loadtxt(ops_path+'tk')
 fomt = np.reshape(np.array(fomt).astype(np.float64),
                   (int(len(fomt)/K), K), order='F')
-tmax = np.loadtxt('tmax')
-tmin = np.loadtxt('tmin')
+tmax = np.loadtxt(ops_path+'tmax')
+tmin = np.loadtxt(ops_path+'tmin')
 ul_bounds = [tmax, tmin]
 
-if min(4, nb) == 1:
-    fig, ax = plt.subplots(min(4, nb), sharex=True, squeeze=True, tight_layout=True)
-    ax.plot(t_grid[i, K:], romt[i, K:], 'b-', mfc="None", label='N = '+str(i))
-    ax.plot(t_grid[i, K:], fomt[i+1, :], 'k-', mfc="None")
-    ax.set_xlabel(r'$t$')
-    ax.legend(loc=0)
-    fig.savefig('./romt/romt.png')
-else:
-    fig, axs = plt.subplots(min(4, nb), sharex=True, squeeze=True, tight_layout=True)
-    for i in range(min(4, nb)):
-        axs[i].plot(t_grid[i, K:], romt[i, K:], 'b-', mfc="None", label='N = '+str(i))
-        axs[i].plot(t_grid[i, K:], fomt[i+1, :], 'k-', mfc="None")
-        axs[i].hlines(y=tmax[i], xmin=t_grid[i, K], xmax=t_grid[i, -1], colors='r')
-        axs[i].hlines(y=tmin[i], xmin=t_grid[i, K], xmax=t_grid[i, -1], colors='r')
-        axs[i].set_xlabel(r'$t$')
-        axs[i].legend(loc=0)
-        axs[i].set_ylabel(r'$T_{'+str(i+1)+'}(t)$')
-    fig.savefig('./romt/romt.png')
-
-tas = np.loadtxt('tas')
-tvs = np.loadtxt('tvs')
+tas = np.loadtxt(ops_path+'tas')
+tvs = np.loadtxt(ops_path+'tvs')
 ta = np.zeros((nb,))
 tv = np.zeros((nb,))
 
 for j in range(nb):
-    ta[j] = np.sum(romt[j, K:])/len(romt[0, K:])
-    tv[j] = np.sum((romt[j, K:]-ta[j])**2)/(len(romt[0, K:])-1)
+    ta[j] = np.sum(romt[j, T0:])/len(romt[0, T0:])
+    tv[j] = np.sum((romt[j, T0:]-ta[j])**2)/(len(romt[0, T0:])-1)
+
+if min(4, nb) == 1:
+    fig, ax = plt.subplots(min(4, nb), sharex=True, squeeze=True, tight_layout=True)
+    ax.plot(t_grid[i, T0:], romt[i, T0:], 'b-', mfc="None", label='ROM')
+    ax.plot(t_grid[i, T0:], fomt[i+1, :], 'k-', mfc="None", label='Snapshot')
+    ax.hlines(y=tmax[i], xmin=t_grid[i, 0], xmax=t_grid[i, -1], colors='r')
+    ax.hlines(y=tmin[i], xmin=t_grid[i, 0], xmax=t_grid[i, -1], colors='r')
+    ax.hlines(y=tas[i+1], xmin=t_grid[i, 0], xmax=t_grid[i, -1], colors='k', linestyle='--', label='Snapshot avg')
+    ax.annotate('Snap std:'+"%.2e"% tvs[i+1], xy=(0, 0.2), xytext=(12, -12), va='top',
+             xycoords='axes fraction', textcoords='offset points')
+    ax.annotate('ROM std:'+"%.2e"% tv[i], xy=(0, 0.25), xytext=(12, -12), va='top',
+             xycoords='axes fraction', textcoords='offset points')
+    ax.set_xlabel(r'$t$')
+    ax.legend(loc=0)
+    ax.set_ylabel(r'$T_{'+str(i+1)+'}(t)$')
+    fig.savefig('./romt/romt_N'+N+'.png')
+else:
+    fig, axs = plt.subplots(min(4, nb), sharex=True, squeeze=True, tight_layout=True)
+    for i in range(min(4, nb)):
+        axs[i].plot(t_grid[i, T0:], romt[i, T0:], 'b-', mfc="None", label='ROM')
+        axs[i].plot(t_grid[i, T0:], fomt[i+1, :], 'k-', mfc="None", label='Snapshot')
+        axs[i].hlines(y=tmax[i], xmin=t_grid[i, 0], xmax=t_grid[i, -1], colors='r')
+        axs[i].hlines(y=tmin[i], xmin=t_grid[i, 0], xmax=t_grid[i, -1], colors='r')
+        axs[i].hlines(y=tas[i+1], xmin=t_grid[i, 0], xmax=t_grid[i, -1], colors='k', linestyle='--', label='Snapshot avg')
+        axs[i].annotate('Snap std:'+"%.2e"% tvs[i+1], xy=(0, 0.2), xytext=(12, -12), va='top',
+                        xycoords='axes fraction', textcoords='offset points')
+        axs[i].annotate('ROM std:'+"%.2e"% tv[i], xy=(0, 0.25), xytext=(12, -12), va='top',
+                        xycoords='axes fraction', textcoords='offset points')
+        axs[i].set_xlabel(r'$t$')
+        axs[i].legend(loc=0)
+        axs[i].set_ylabel(r'$T_{'+str(i+1)+'}(t)$')
+    fig.savefig('./romt/romt_N'+N+'.png')
+
+
 
 fig, axs = plt.subplots(2, sharex=True, tight_layout=True)
 
@@ -145,4 +165,4 @@ for i in range(2):
     if i == 1:
         axs[i].set_yscale('log')
 
-fig.savefig('./romt/ta_tv.png')
+fig.savefig('./romt/ta_tv_N'+N+'.png')
