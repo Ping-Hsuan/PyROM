863e1b9254fcab3a1d9a28ba85ee849721f0a082
Sean <r04246004@ntu.edu.tw>
2021-02-14 17:17:38 -0600

    add modules

---
+++ code/plot_helpers/reader.py
@@ -0,0 +1,7 @@
+def reader(fname):
+    with open(fname, 'r') as f:
+        k = f.read()
+    list_of_lines = k.split('\n')
+    list_of_words = [[k for k in line.split(' ') if k] for line in list_of_lines][:-1]
+    data = [x[-1] for x in list_of_words]
+    return data
