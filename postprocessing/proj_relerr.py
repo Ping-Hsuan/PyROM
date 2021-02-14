180f8aaf4fcbc7d92b60364e6e12f9189c9404e6
Sean <r04246004@ntu.edu.tw>
2021-02-14 17:20:05 -0600

    specify N when running proj_relerr.py

--- postprocessing/proj_relerr.py
+++ postprocessing/proj_relerr.py
@@ -19,9 +19,8 @@ matplotlib.rcParams['text.latex.preamble'] = [
 
 print("This is the name of the program:", sys.argv[0])
 print("Argument List:", str(sys.argv))
-print(os.getcwd())
 os.chdir(str(sys.argv[1]))
-print(os.getcwd())
+N = int(sys.argv[2])
 isExist = os.path.exists(os.getcwd()+'/proj_relerr/')
 if isExist:
     pass
@@ -64,43 +63,46 @@ for nb, fnames in dict_final:
     angle = []
     data = []
     merr_proj = []
-    for fname in fnames:
-        forleg = fname.split('_')
-        pl = 1
+    if nb == N:
+        for fname in fnames:
+            forleg = fname.split('_')
+            pl = 1
 
-        match_rom = re.match('^.*_(.*)rom_.*$', fname)
-        assert match_rom is not None
+            match_rom = re.match('^.*_(.*)rom_.*$', fname)
+            assert match_rom is not None
 
-        if match_rom.groups()[0] == '':
-            solver = 'Galerkin ROM'
-        elif match_rom.groups()[0] == 'c':
-            solver = 'Constrained ROM'
-        elif match_rom.groups()[0] == 'l':
-            solver = 'Leray ROM'
+            if match_rom.groups()[0] == '':
+                solver = 'Galerkin ROM'
+                angle.append(int(forleg[-3])+90)
+            elif match_rom.groups()[0] == 'c':
+                solver = 'Constrained ROM'
+                angle.append(int(forleg[-3])+90)
+            elif match_rom.groups()[0] == 'l':
+                solver = 'Leray ROM'
+                angle.append(int(forleg[-4])+90)
 
 
-        with open(tpath+fname, 'r') as f:
-              k = f.read()
-        list_of_lines = k.split('\n')
-        list_of_words = [[k for k in line.split(' ') if k and k != 'dual' and k != 'norm:'] for line in list_of_lines][:-1]
-        data = [x[-1] for x in list_of_words]
-        data.pop(0)
-        data = np.array(data).astype(np.float64)
-        merr_proj.append(data)
-        angle.append(int(forleg[-3])+90)
+            with open(tpath+fname, 'r') as f:
+                  k = f.read()
+            list_of_lines = k.split('\n')
+            list_of_words = [[k for k in line.split(' ') if k and k != 'dual' and k != 'norm:'] for line in list_of_lines][:-1]
+            data = [x[-1] for x in list_of_words]
+            data.pop(0)
+            data = np.array(data).astype(np.float64)
+            merr_proj.append(data)
 
-    data = np.column_stack((angle, merr_proj))
-    data = data[data[:, 0].argsort()]
-    fig, ax = plt.subplots(1, tight_layout=True)
-    ax.semilogy(data[:, 0], data[:, 1], 'k-o',
-                mfc="None", label=r'$N = $'+str(nb)+', '+r'$\theta^*_g = '+str(int(anchor))+'$')
+        data = np.column_stack((angle, merr_proj))
+        data = data[data[:, 0].argsort()]
+        fig, ax = plt.subplots(1, tight_layout=True)
+        ax.semilogy(data[:, 0], data[:, 1], 'k-o',
+                    mfc="None", label=r'$N = $'+str(nb)+', '+r'$\theta^*_g = '+str(int(anchor))+'$')
 
-    ax.set_ylabel(r'$\frac{\|u - \mathcal{P}u\|_{H^1}}{\|u\|_{H^1}}$')
-    ax.set_xlabel(r'$\theta_g$')
-    ax.set_xticks(np.linspace(0, 180, 5, dtype=int))
-    ax.set_ylim([1e-3, 1])
-    ax.legend(loc=0, ncol=2)
-    fig.savefig(tpath+'proj_relerr_N'+str(nb)+'.png')
-    np.savetxt(tpath+'angle.dat', data[:, 0])
-    np.savetxt(tpath+'proj_relerr_N'+str(nb)+'.dat', data[:, 1])
+        ax.set_ylabel(r'$\frac{\|u - \mathcal{P}u\|_{H^1}}{\|u\|_{H^1}}$')
+        ax.set_xlabel(r'$\theta_g$')
+        ax.set_xticks(np.linspace(0, 180, 5, dtype=int))
+        ax.set_ylim([1e-3, 1])
+        ax.legend(loc=0, ncol=2)
+        fig.savefig(tpath+'proj_relerr_N'+str(nb)+'.png')
+        np.savetxt(tpath+'angle.dat', data[:, 0])
+        np.savetxt(tpath+'proj_relerr_N'+str(nb)+'.dat', data[:, 1])
 
