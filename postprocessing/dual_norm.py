4f7f730df84bc0308badab716010f54ede5d0e7a
Sean <r04246004@ntu.edu.tw>
2021-02-14 17:17:59 -0600

    update dual_norm.py

--- postprocessing/dual_norm.py
+++ postprocessing/dual_norm.py
@@ -1,69 +1,42 @@
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 import re
 import os
 import sys
 import operator
+sys.path.append('/Users/bigticket0501/Developer/PyMOR/code/plot_helpers/')
+import setup
+import reader
 
-plt.style.use('report')
-
-matplotlib.rcParams['text.latex.preamble'] = [
-       r'\usepackage{siunitx}',   # i need upright \micro symbols, but you need...
-       r'\sisetup{detect-all}',   # ...this to force siunitx to actually use your fonts
-       r'\usepackage{helvet}',    # set the normal font here
-       r'\usepackage{sansmath}',  # load up the sansmath so that math -> helvet
-       r'\sansmath'               # <- tricky! -- gotta actually tell tex to use!
-]
+setup.style(1)
+colors = setup.color(0)
+setup.text()
 
 print("This is the name of the program:", sys.argv[0])
 print("Argument List:", str(sys.argv))
 os.chdir(str(sys.argv[1]))
-N = int(sys.argv[2])
-isExist = os.path.exists(os.getcwd()+'/dual_norm/')
-if isExist:
-    pass
-else:
-    os.mkdir(os.getcwd()+'/dual_norm/')
+N = str(sys.argv[2])
+print(os.getcwd())
+target_dir = '/dual_norm'
 
-for root, dirs, files in os.walk("./dual_norm/", topdown=False):
-    for name in files:
-        if re.match('^.*_(.*)rom_.*$', name):
-            print(os.path.join(root, name))
-    for name in dirs:
-        print(os.path.join(root, name))
+setup.checkdir(target_dir)
 
-filenames = [name for name in files if re.match('^.*_(.*)rom_.*$', name)]
+root, filenames = setup.gtfpath(target_dir, '^.*_(.*)rom_.*$')
+files_dict = setup.create_dict(filenames, '^.*_([0-9]*)nb_.*$')
 
-dic_for_files = {}
+dict_final = sorted(files_dict.items(), key=operator.itemgetter(0))
 
-for fname in filenames:
-    match_nb = re.match('^.*_([0-9]*)nb_.*$', fname)
-    if match_nb:
-        if int(match_nb.groups()[0]) not in dic_for_files:
-            dic_for_files[int(match_nb.groups()[0])] = []
-        dic_for_files[int(match_nb.groups()[0])].append(fname)
+anchor = setup.find_anchor()
 
-colors = cm.Set1(np.linspace(0, 1, 9))
 color_ctr = 0
-
-dict_final = sorted(dic_for_files.items(), key=operator.itemgetter(0))
-
-root = os.getcwd()
-sp1 = (root.split('/'))
-for element in sp1:
-    z = re.match(r"theta_(\d+)", element)
-    if z:
-        anchor = float(((z.groups())[0]))
-
-tpath = './dual_norm/'
+tpath = root+'/'
 
 for nb, fnames in dict_final:
     erri = []
     angle = []
     data = []
     if nb == N:
+        print(nb)
         for fname in fnames:
             forleg = fname.split('_')
             pl = 1
@@ -73,20 +46,16 @@ for nb, fnames in dict_final:
 
             if match_rom.groups()[0] == '':
                 solver = 'Galerkin ROM'
+                angle.append(int(forleg[-3])+90)
             elif match_rom.groups()[0] == 'c':
                 solver = 'Constrained ROM'
+                angle.append(int(forleg[-3])+90)
             elif match_rom.groups()[0] == 'l':
                 solver = 'Leray ROM'
+                angle.append(int(forleg[-4])+90)
 
-            with open(tpath+fname, 'r') as f:
-                  k = f.read()
-            list_of_lines = k.split('\n')
-            list_of_words = [[k for k in line.split(' ') if k and k != 'dual' and k != 'norm:'] for line in list_of_lines][:-1]
-            data = [x[-1] for x in list_of_words]
-            dual_norm = np.array(data).astype(np.float64)
-
-            erri.append(float(dual_norm))
-            angle.append(int(forleg[-3])+90)
+            data = reader.reader(fname)
+            erri.append(np.array(data).astype(np.float64))
 
         data = np.column_stack((angle, erri))
         data = data[data[:, 0].argsort()]
@@ -99,6 +68,7 @@ for nb, fnames in dict_final:
         ax.set_xticks(np.linspace(0, 180, 5, dtype=int))
         ax.set_ylim([1e-2, 1])
         ax.legend(loc=0, ncol=2)
+        print(os.getcwd())
         fig.savefig(tpath+'online_N'+str(nb)+'.png')
         np.savetxt(tpath+'angle.dat', data[:, 0])
         np.savetxt(tpath+'erri_N'+str(nb)+'.dat', data[:, 1])
