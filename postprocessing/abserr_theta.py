c51f1ce3b0cc5cc801d166fdbddfb25463a9fedd
Sean <r04246004@ntu.edu.tw>
2021-02-15 17:09:03 -0600

    clean up fld relerr and abserr

--- postprocessing/abserr_theta.py
+++ postprocessing/abserr_theta.py
@@ -1,21 +1,16 @@
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-import re
 import os
 import sys
 import operator
+sys.path.append('/Users/bigticket0501/Developer/PyMOR/code/plot_helpers/')
+import setup
+import reader
+import checker
 
-plt.style.use('report_1fig')
-
-matplotlib.rcParams['text.latex.preamble'] = [
-       r'\usepackage{siunitx}',   # i need upright \micro symbols, but you need...
-       r'\sisetup{detect-all}',   # ...this to force siunitx to actually use your fonts
-       r'\usepackage{helvet}',    # set the normal font here
-       r'\usepackage{sansmath}',  # load up the sansmath so that math -> helvet
-       r'\sansmath'               # <- tricky! -- gotta actually tell tex to use!
-]
+setup.style(1)
+colors = setup.color(0)
+setup.text()
 
 print("---------------------------------------------")
 print("This is the name of the program:", sys.argv[0])
@@ -24,82 +19,40 @@ os.chdir(str(sys.argv[1]))
 deg = str(int(sys.argv[2])-90)
 print("---------------------------------------------")
 
-isExist = os.path.exists(os.getcwd()+'/rom_abserr/')
-if isExist:
-    print("The target directory exist")
-    pass
-else:
-    os.mkdir(os.getcwd()+'/rom_abserr/')
-    print("Create the target directory successfully")
-print("---------------------------------------------")
+target_dir = '/rom_abserr'
+setup.checkdir(target_dir)
 
-for root, dirs, files in os.walk("./rom_abserr/", topdown=False):
-    for name in files:
-        if re.match('^.*_(.*)rom_.*$', name):
-            pass
-#           print(os.path.join(root, name))
-    for name in dirs:
-        pass
-#       print(os.path.join(root, name))
+root, filenames = setup.gtfpath(target_dir, '^.*_h10_'+deg+'_.*$')
+files_dict = setup.create_dict(filenames, '^.*_([0-9]*)nb_.*$')
+dict_final = sorted(files_dict.items(), key=operator.itemgetter(0))
 
-filenames = [name for name in files if re.match('^.*_h10_'+deg+'_.*$', name)]
-
-dic_for_files = {}
-for fname in filenames:
-    match_nb = re.match('^.*_([0-9]*)nb_.*$', fname)
-    if match_nb:
-        if int(match_nb.groups()[0]) not in dic_for_files:
-            dic_for_files[int(match_nb.groups()[0])] = []
-        dic_for_files[int(match_nb.groups()[0])].append(fname)
-
-colors = cm.tab10(np.linspace(0, 1, 10))
 color_ctr = 0
-
-dict_final = {k: v for k, v in sorted(dic_for_files.items(), key=operator.itemgetter(0))}
-
-i = 0
-tpath = './rom_abserr/'
+tpath = root+'/'
 
 N_list = []
 data = []
 merr_rom = []
 merr_proj = []
-for nb in dict_final:
-    fname = (dict_final.get(nb)).pop()
-
-    match_rom = re.match('^.*_(.*)rom_.*$', fname)
-    assert match_rom is not None
-
-    if match_rom.groups()[0] == '':
-        solver = 'Galerkin ROM'
-    elif match_rom.groups()[0] == 'c':
-        solver = 'Constrained ROM'
-    elif match_rom.groups()[0] == 'l':
-        solver = 'Leray ROM'
-
-    with open(tpath+fname, 'r') as f:
-        k = f.read()
-    list_of_lines = k.split('\n')
-    list_of_words = [[k for k in line.split(' ') if k and k != 'dual'
-                     and k != 'norm:'] for line in list_of_lines][:-1]
-    data = [x[-1] for x in list_of_words]
-    data = np.array(data).astype(np.float64)
-    merr_rom.append(data[0])
-    merr_proj.append(data[1])
-    N_list.append(nb)
+for nb, fnames in dict_final:
+    for fname in fnames:
+        data = reader.reader(fname)
+        data = np.array(data).astype(np.float64)
+        merr_rom.append(data[0])
+        merr_proj.append(data[1])
+        N_list.append(int(nb))
 
 data = np.column_stack((N_list, merr_rom, merr_proj))
 data = data[data[:, 0].argsort()]
 
 fig, ax = plt.subplots(1, tight_layout=True)
-ax.plot(data[:, 0], data[:, 1], 'b-o', mfc="None")
-ax.plot(data[:, 0], data[:, 2], 'k-o', mfc="None")
+ax.set(ylabel=r'$\|u_{avg} - \widehat{u}_{avg}\|_{H^1}$', xlabel=r'$N$',
+       xlim=[0, max(data[:, 0])], title='Absolute error in the ' +
+       r'mean flow at $\theta_g='+str(int(deg)+90)+'$')
 
-ax.set_title(r'Absolute error in the mean flow at $\theta_g='+str(int(deg)+90)+'$')
-ax.set_ylabel(r'$\|u_{avg} - \widehat{u}_{avg}\|_{H^1}$')
-ax.set_xlabel(r'$N$')
-ax.set_xlim([0, max(data[:, 0])])
-#ax.legend(loc=0)
+plot_params1 = {'c': 'b', 'marker': 'o', 'mfc': 'None'}
+plot_params2 = {'c': 'k', 'marker': 'o', 'mfc': 'None'}
+ax.plot(data[:, 0], data[:, 1], **plot_params1)
+ax.plot(data[:, 0], data[:, 2], **plot_params2)
 
 print("---------------------------------------------")
 fig.savefig(tpath+'abserr_theta_'+str(int(deg)+90)+'.png')
