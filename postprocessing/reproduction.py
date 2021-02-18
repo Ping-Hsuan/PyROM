1172863b719181fbf4744d0b9bb26cceea627668
Sean <r04246004@ntu.edu.tw>
2021-02-17 23:03:05 -0600

    add script to do reproduction

---
+++ postprocessing/reproduction.py
@@ -0,0 +1,20 @@
+import sys
+import subprocess
+sys.path.append('/Users/bigticket0501/Developer/PyMOR/code/plot_helpers/')
+# This python script is used to plot reproduction results
+# You must the model and the parameter theta_g
+# (for now only support for theta_g)
+
+tpath = sys.argv[1]
+model = sys.argv[2]
+theta_g = sys.argv[3]
+T0 = sys.argv[4]
+N = sys.argv[5]
+
+subprocess.run(["python3", "dual_norm_wN.py", tpath, theta_g])
+subprocess.run(["python3", "romt.py", tpath, model, theta_g, N])
+subprocess.run(["python3", "romu.py", tpath, model, theta_g, N])
+subprocess.run(["python3", "relerr_wN.py", tpath, theta_g])
+subprocess.run(["python3", "abserr_wN.py", tpath, theta_g])
+subprocess.run(["python3", "rom_norm_wN.py", tpath, theta_g])
+subprocess.run(["python3", "nu_first_second_momentum_wN.py", tpath, theta_g, T0])
