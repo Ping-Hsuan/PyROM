bb531e49d9c9517084a87619a3640e40aea4eec9
Sean <r04246004@ntu.edu.tw>
2021-02-17 23:01:30 -0600

    add script to do reproduction

--- postprocessing/plot_data_wN.py
+++ postprocessing/plot_data_wN.py
@@ -6,7 +6,8 @@ tpath = sys.argv[1]
 theta = sys.argv[2]
 T0 = sys.argv[3]
 
-subprocess.run(["python3", "dual_norm_theta.py", tpath, theta])
-subprocess.run(["python3", "relerr_theta.py", tpath, theta])
-subprocess.run(["python3", "abserr_theta.py", tpath, theta])
-subprocess.run(["python3", "mnu_theta.py", tpath, theta, T0])
+subprocess.run(["python3", "dual_norm_wN.py", tpath, theta])
+subprocess.run(["python3", "relerr_wN.py", tpath, theta])
+subprocess.run(["python3", "abserr_wN.py", tpath, theta])
+subprocess.run(["python3", "rom_norm_wN.py", tpath, theta])
+subprocess.run(["python3", "nu_first_second_momentum_wN.py", tpath, theta, T0])
