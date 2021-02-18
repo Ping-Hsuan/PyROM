990003338f32a4b9c105f1e6c09712c416cac52c
Sean <r04246004@ntu.edu.tw>
2021-02-17 23:00:58 -0600

    fix relerr_wN.py

--- postprocessing/relerr_wN.py
+++ postprocessing/relerr_wN.py
@@ -16,35 +16,35 @@ print("---------------------------------------------")
 print("This is the name of the program:", sys.argv[0])
 print("Argument List:", str(sys.argv))
 os.chdir(str(sys.argv[1]))
-deg = str(int(sys.argv[2])-90)
+model = str(sys.argv[2])
+deg = str(int(sys.argv[3])-90)
 print("---------------------------------------------")
 
-target_dir = '/proj_relerr'
+target_dir = '/relerr/'
 setup.checkdir(target_dir)
 
-root, filenames = setup.gtfpath(target_dir, '^.*_h10_'+deg+'_.*$')
-tpath = root+'/'
+search_dir = './'+model+'_info/proj_relerr'
+root, filenames = setup.gtfpath(search_dir, '^.*_h10_'+deg+'_.*$')
 files_dict = setup.create_dict(filenames, '^.*_([0-9]*)nb_.*$')
 dict_final = sorted(files_dict.items(), key=operator.itemgetter(0))
 
 color_ctr = 0
-
-anchor = setup.find_anchor()
+tpath = root+'/'
 
 N_list = []
 data = []
-merr_proj = []
-merr_rom = []
+mrelerr_proj = []
+mrelerr_rom = []
 for nb, fnames in dict_final:
     for fname in fnames:
         solver = checker.rom_checker(fname, '^.*_(.*)rom_.*$')
         data = reader.reader(fname)
         data = np.array(data).astype(np.float64)
-        merr_rom.append(data[0])
-        merr_proj.append(data[1])
+        mrelerr_rom.append(data[0])
+        mrelerr_proj.append(data[1])
         N_list.append(int(nb))
 
-data = np.column_stack((N_list, merr_rom, merr_proj))
+data = np.column_stack((N_list, mrelerr_rom, mrelerr_proj))
 data = data[data[:, 0].argsort()]
 
 plot_params1 = {'c': 'b', 'marker': 'o', 'mfc': 'None',
@@ -63,9 +63,9 @@ ax.semilogy(data[:, 0], data[:, 2], **plot_params2)
 ax.legend(loc=0, ncol=1)
 
 print("---------------------------------------------")
-fig.savefig(tpath+'relerr_theta_'+str(int(deg)+90)+'.png')
-np.savetxt(tpath+'N_list_'+str(int(deg)+90)+'.dat', data[:, 0])
-np.savetxt(tpath+'rom_relerr_theta_'+str(int(deg)+90)+'.dat', data[:, 1])
-np.savetxt(tpath+'proj_relerr_theta_'+str(int(deg)+90)+'.dat', data[:, 2])
+fig.savefig('.'+target_dir+'relerr_theta_'+str(int(deg)+90)+'.png')
+np.savetxt('.'+target_dir+'N_list_'+str(int(deg)+90)+'.dat', data[:, 0])
+np.savetxt('.'+target_dir+'rom_relerr_theta_'+str(int(deg)+90)+'.dat', data[:, 1])
+np.savetxt('.'+target_dir+'proj_relerr_theta_'+str(int(deg)+90)+'.dat', data[:, 2])
 print("---------------------------------------------")
 
