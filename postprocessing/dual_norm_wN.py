ff67502b9d5565d1ea9b10c8536ab2df9acda62b
Sean <r04246004@ntu.edu.tw>
2021-03-14 15:07:33 -0500

    load data with ROM class

--- postprocessing/dual_norm_wN.py
+++ postprocessing/dual_norm_wN.py
@@ -7,6 +7,9 @@ sys.path.append('/Users/bigticket0501/Developer/PyMOR/code/plot_helpers/')
 import setup
 import reader
 import checker
+from mor import ROM
+from snapshot import Snapshot
+from aux import sort
 
 setup.style(1)
 colors = setup.color(0)
@@ -31,33 +34,30 @@ dict_final = sorted(files_dict.items(), key=operator.itemgetter(0))
 color_ctr = 0
 tpath = root+'/'
 
-erri = []
-N_list = []
+roms = []
 for nb, fnames in dict_final:
     for fname in fnames:
-        data = reader.reader(fname)
-        dual_norm = np.array(data).astype(np.float64)
-        erri.append(float(dual_norm))
-        N_list.append(int(nb))
+        rom = ROM(fname)
+        rom.DTAR()
+        rom.anchor('theta')
+        roms.append(rom)
+data = sort(roms, 'nb', 'dtar')
+solver = rom.info['method']
+anchor = str(int(rom.info['anchor']))
 
-anchor = setup.find_anchor()
-solver = checker.rom_checker(fname, '^.*_(.*)rom_.*$')
-data = np.column_stack((N_list, erri))
-data = data[data[:, 0].argsort()]
-
-if len(N_list) == 1:
+if len(data[:, 0]) == 1:
     pass
 else:
     fig, ax = plt.subplots(1, tight_layout=True)
     plot_params = {'c': 'k', 'marker': 'o', 'mfc': 'None',
-                   'label': solver+' with '+r'$\theta^*_g = '+str(int(anchor))+'$'}
+                   'label': solver+' with '+r'$\theta^*_g = '+anchor+'$'}
     ax.set(ylabel=r'$\triangle(\theta_g='+str(int(deg)+90)+')$', xlabel=r'$N$',
-           ylim=[1e-2, 1], xlim=[1, max(data[:, 0])])
+            ylim=[10**np.floor(np.log10(min(data[:, 1]))), 1], xlim=[1, max(data[:, 0])])
     ax.semilogy(data[:, 0], data[:, 1], **plot_params)
 
     print("---------------------------------------------")
     fig.savefig('.'+target_dir+'dual_norm_theta_'+str(int(deg)+90)+'.png')
     print("---------------------------------------------")
-
+plt.show()
 np.savetxt('.'+target_dir+'N_list_'+str(int(deg)+90)+'.dat', data[:, 0])
 np.savetxt('.'+target_dir+'erri_theta_'+str(int(deg)+90)+'.dat', data[:, 1])
