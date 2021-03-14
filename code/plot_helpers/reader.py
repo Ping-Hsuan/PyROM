f43d0e45de28352a7fbc77ff23fa0b90f79bd01c
Sean <r04246004@ntu.edu.tw>
2021-03-14 15:08:51 -0500

    udpate reader

--- code/plot_helpers/reader.py
+++ code/plot_helpers/reader.py
@@ -5,3 +5,25 @@ def reader(fname):
     list_of_words = [[k for k in line.split(' ') if k] for line in list_of_lines][:-1]
     data = [x[-1] for x in list_of_words]
     return data
+
+
+def erri_w_theta(model, anchor, N, *argv):
+    import numpy as np
+    if len(argv) == 0:
+        fname = 'erri_N'+N+'.dat'
+    else:
+        if str(argv[0]) == 'fom':
+            fname = 'erri_N'+N+'_sc_fom.dat'
+        elif str(argv[0]) == 'romabserr':
+            fname = 'erri_N'+N+'_sc_romabserr.dat'
+        elif str(argv[0]) == 'rom':
+            fname = 'erri_N'+N+'_sc_rom.dat'
+
+    print(fname)
+    filename = '../theta_'+str(anchor)+'/'+model+'_parameter_'+str(anchor)+'/dual_norm/angle_list.dat'
+    angle = np.loadtxt(filename)
+    filename = '../theta_'+str(anchor)+'/'+model+'_parameter_' + \
+               str(anchor)+'/'+'dual_norm/'+fname
+    erri = np.loadtxt(filename)
+
+    return angle, erri
