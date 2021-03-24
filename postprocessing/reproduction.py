eb528d3ff4218602519e4d49ee246d7157a0590c
Sean <r04246004@ntu.edu.tw>
2021-03-24 18:27:38 -0500

    grep data when call reproduction

--- postprocessing/reproduction.py
+++ postprocessing/reproduction.py
@@ -1,16 +1,25 @@
 import sys
+import os
 import subprocess
 sys.path.append('/Users/bigticket0501/Developer/PyMOR/code/plot_helpers/')
 # This python script is used to plot reproduction results
 # You must the model and the parameter theta_g
 # (for now only support for theta_g)
 
-tpath = sys.argv[1]
+setup_path = sys.argv[1]
 model = sys.argv[2]
 theta_g = sys.argv[3]
 T0 = sys.argv[4]
 N = sys.argv[5]
 
+tpath = setup_path+'/'+model+'_reproduction_'+str(theta_g)
+isExist = os.path.exists(tpath)
+if isExist:
+    pass
+else:
+    os.mkdir(tpath)
+
+subprocess.run(["python3", "grep_data.py", setup_path, model])
 subprocess.run(["python3", "dual_norm_wN.py", tpath, model, theta_g])
 subprocess.run(["python3", "romt.py", tpath, model, theta_g, N])
 subprocess.run(["python3", "romu.py", tpath, model, theta_g, N])
