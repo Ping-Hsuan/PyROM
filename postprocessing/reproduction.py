4b81e53506632ce90ca161c3220fdaa86324080e
Sean <r04246004@ntu.edu.tw>
2021-04-01 00:41:06 -0500

    still working on reproduction.py

--- postprocessing/reproduction.py
+++ postprocessing/reproduction.py
@@ -1,29 +1,57 @@
 import sys
 import os
 import subprocess
-sys.path.append('/Users/bigticket0501/Developer/PyMOR/code/plot_helpers/')
+import operator
+import yaml
+#sys.path.append('/Users/bigticket0501/Developer/PyMOR/code/plot_helpers/')
+sys.path.append('/Users/bigticket0501/Developer/PyMOR/code/post_pro/')
+import aux
+from mor import ROM
+
 # This python script is used to plot reproduction results
 # You must the model and the parameter theta_g
 # (for now only support for theta_g)
 
-setup_path = sys.argv[1]
-model = sys.argv[2]
-theta_g = sys.argv[3]
-T0 = sys.argv[4]
-N = sys.argv[5]
+# Extract user specify features
+with open('reproduction.yaml') as f:
+    info = yaml.load(f, Loader=yaml.FullLoader)
 
-tpath = setup_path+'/'+model+'_reproduction_'+str(theta_g)
+rom = ROM(info)
+rom.get_data()
+rom.field = 'u'
+rom.get_coef()
+rom.compute_momentum()
+aux.plt_coef_in_t(rom)
+1/o
+setup_path = os.getcwd()
+model = info['formulation']
+
+dir1 = model+'_reproduction'
+for key, value in info['parameters'].items():
+    dir1 += '_'+str(key)+'_'+str(value)
+T0 = info['T0']
+N = info['nb']
+
+tpath = os.path.join(setup_path, dir1)
 isExist = os.path.exists(tpath)
 if isExist:
     pass
 else:
     os.mkdir(tpath)
 
-subprocess.run(["python3", "grep_data.py", setup_path, model])
-subprocess.run(["python3", "dual_norm_wN.py", tpath, model, theta_g])
-subprocess.run(["python3", "romt.py", tpath, model, theta_g, N])
+for feature in info['features'].keys():
+    search_dir = model+'_info'
+    fnames = aux.gtfpath(search_dir, '^.*_h10_.*_'+feature)
+#   files_dict = aux.create_dict(fnames, '^.*_([0-9]*)nb_.*$')
+    # sort the dictionary key
+#   dict_final = sorted(files_dict.items(), key=operator.itemgetter(0))
+    data = aux.get_data(fnames, feature, info)
+
+1/o
+#subprocess.run(["python3", "grep_data.py", setup_path, model])
+#subprocess.run(["python3", "romt.py", tpath, model, theta_g, N])
 subprocess.run(["python3", "romu.py", tpath, model, theta_g, N])
-subprocess.run(["python3", "relerr_wN.py", tpath, model, theta_g])
-subprocess.run(["python3", "abserr_wN.py", tpath, model, theta_g])
-subprocess.run(["python3", "rom_norm_wN.py", tpath, model, theta_g])
-subprocess.run(["python3", "nu_first_second_momentum_wN.py", tpath, model, theta_g, T0])
+#subprocess.run(["python3", "relerr_wN.py", tpath, model, theta_g])
+#subprocess.run(["python3", "abserr_wN.py", tpath, model, theta_g])
+#subprocess.run(["python3", "rom_norm_wN.py", tpath, model, theta_g])
+#subprocess.run(["python3", "nu_first_second_momentum_wN.py", tpath, model, theta_g, T0])
