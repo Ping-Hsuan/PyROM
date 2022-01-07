3333688bb783fcfa3d5e91f4229e4814e47bff79
Ping-Hsuan <pht2@illinois.edu>
2022-01-07 12:34:34 -0600

    update convergence compare script

--- postprocessing/compare/conv_compare.py
+++ postprocessing/compare/conv_compare.py
@@ -1,12 +1,15 @@
 def conv_compare(argv):
     import yaml
     import os
+    import re
     import pandas as pd
     import numpy as np
     import matplotlib.pyplot as plt
     from figsetup.style import style
     from figsetup.text import text
     from aux.create_dir import create_dir
+    sys.path.append('/home/pht2/Developer/PyROM/code/plot_helpers/')
+    import reader
 
     style(1)
     text()
@@ -22,7 +25,7 @@ def conv_compare(argv):
             info = yaml.load(f, Loader=yaml.FullLoader)
         for key, value in info['parameters'].items():
             al = '_'.join([str(key), str(value)])
-        dir1 = '_'.join([dir1, al])
+            dir1 = '_'.join([dir1, al])
         if model == 'l-rom':
             fd = info['perc'].replace('p', '.')
             fd = str(int(float(fd)*100))
@@ -35,19 +38,64 @@ def conv_compare(argv):
         else:
             solver = model.upper()
             fname = feature+'.csv'
-        tpath = os.path.join(dir1, feature, fname)
-        data = pd.read_csv(tpath)
+        tpath = reader.find_files(fname, './')
+        print(tpath)
+        for f in tpath:
+            if model in re.split('[/_]', f):
+                fn = f
+#       tpath = os.path.join(dir1, feature, fname)
+        data = pd.read_csv(fn)
         ax.plot(data.iloc[:, 0], data.iloc[:, 1], '-o', label=solver)
     ax.legend(loc=0)
-    anc_lb = ''
+    anc_lb = []
     for key, value in info['parameters'].items():
         if key == 'theta':
-            anc_lb += '\\'+str(key)+'^*_g='+str(value)
+            anc_lb.append('\\'+str(key)+'^*_g='+str(value))
         else:
-            anc_lb += str(key)+'^*='+str(value)
-    title = 'Relative error in the predicted mean flow at '+'$'+anc_lb+'$'
-    ax.set(xlabel=r'$N$', ylabel=r'$\frac{\|\langle \bf{u} - \bf{\tilde{u}} \rangle\|_{H^1}}{\|\langle \bf{u} \rangle\|_{H^1}}$',
-           ylim=[0, 0.7], title=title)
+            anc_lb.append(str(key)+'^*='+str(value))
+    anc_lb = ', '.join(anc_lb)
+    if feature == 'mrelerr_h1':
+       title = 'Relative error in the predicted mean flow at '+'$'+anc_lb+'$'
+       ax.set(xlabel=r'$N$', ylabel=r'$\frac{\|\langle \bf{u} - \bf{\tilde{u}} \rangle\|_{H^1}}{\|\langle \bf{u} \rangle\|_{H^1}}$',
+              ylim=[1e-3, 1], title=title)
+       ax.set_yscale('log')
+    elif feature == 'mtke':
+       title = 'Predicted mean TKE at '+'$'+anc_lb+'$'
+       ax.set(xlabel=r'$N$', ylabel=r'$\langle TKE \rangle_g$',
+              title=title)
+       ax.set_yscale('log')
+       mtke_fom = np.loadtxt('../qoi/tmtke')
+       fom_params = {'c': 'k', 'marker': 'o','label':'FOM'}
+       ax.plot(data.iloc[:, 0], mtke_fom*np.ones(len(data.iloc[:, 0])), **fom_params)
+    elif feature == 'dual_norm':
+       title = 'Dual norm at '+'$'+anc_lb+'$'
+       ax.set(xlabel=r'$N$', ylabel=r'$\triangle$', title=title, ylim=[1e-4, 1])
+       ax.set_yscale('log')
+    elif feature == 'mtfluc':
+       title = 'Predicted mean fluctuation in temperature at '+'$'+anc_lb+'$'
+       ax.set(xlabel=r'$N$', ylabel=r'$\langle T_{fluc} \rangle_s$', title=title)
+       mtfluc_fom = np.loadtxt('../qoi/tmtfluc')
+       fom_params = {'c': 'k', 'marker': 'o','label':'FOM'}
+       ax.plot(data.iloc[:, 0], mtfluc_fom*np.ones(len(data.iloc[:, 0])), **fom_params)
+    elif feature == 'mnu':
+       filename = './fom/nus_mom.csv'
+       fom = pd.read_csv(filename).to_numpy()
+       title = 'Predicted mean Nu at '+'$'+anc_lb+'$'
+       ax.set(xlabel=r'$N$', ylabel=r'$\langle Nu \rangle_s$', title=title)
+       fom_params = {'c': 'k', 'marker': 'o','label':'FOM'}
+       ax.plot(data.iloc[:, 0], fom[0][0]*np.ones(len(data.iloc[:, 0])), **fom_params)
+    elif feature == 'stdnu':
+       filename = './fom/nus_mom.csv'
+       fom = pd.read_csv(filename).to_numpy()
+       title = 'Predicted std(Nu) at '+'$'+anc_lb+'$'
+       ax.set(xlabel=r'$N$', ylabel=r'Std(Nu)', title=title)
+       fom_params = {'c': 'k', 'marker': 'o','label':'FOM'}
+       ax.plot(data.iloc[:, 0], fom[0][1]*np.ones(len(data.iloc[:, 0])), **fom_params)
+    elif feature == 'mnu_err':
+       title = 'Relative error in mean Nu at '+'$'+anc_lb+'$'
+       ax.set(xlabel=r'$N$', title=title)
+       ax.set_yscale('log')
+    ax.legend(loc=0)
     tdir = './compare/'
     create_dir(tdir)
     fig.savefig(tdir+feature+'_conv_compare.png')
