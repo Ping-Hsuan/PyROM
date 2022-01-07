b6af6557e21bacd15b0b7777ac422c00b51ef349
Sean <r04246004@ntu.edu.tw>
2022-01-07 12:45:43 -0600

    update miscellaneous script

--- code/plot_helpers/online.py
+++ code/plot_helpers/online.py
@@ -39,6 +39,9 @@ def get_ncand(P_test, P_train, ncand, N, model, mode, scaled=''):
 
 def get_ncand_param(P_test, P_train, ncand, N, model, mode, fd, scaled=''):
     import numpy as np
+    import pandas as pd
+    import reader
+    import re
 
     P_test_anchor = []
     # Find out the nearest anchor(s) point to each testing parameter
@@ -65,15 +68,24 @@ def get_ncand_param(P_test, P_train, ncand, N, model, mode, fd, scaled=''):
 #           else:
 #               tmp[minidx] = 1e8
 
-            dirs = '../ra_'+str(int(P_train[minidx]))+'_theta_90/' + \
-                   model[minidx]+'_parameter_ra_'+str(int(P_train[minidx]))+'/dual_norm/'
             if model[minidx] == 'l-rom':
                 fname = 'erri_N'+str(N[minidx])+'_'+fd[minidx].strip('0')+scaled+'_'+mode+'.dat'
+            elif model[minidx] == 'l-rom-df':
+                fname = 'dual_norm_N'+str(N[minidx])+'_0'+fd[minidx]+scaled+'.csv'
             else:
                 fname = 'erri_N'+str(N[minidx])+scaled+'_'+mode+'.dat'
-            dual_norm = np.loadtxt(dirs+fname)
-            data = np.array(dual_norm).astype(np.float64)
-            erri.append(data[i])
+                fname = 'dual_norm_N'+str(N[minidx])+scaled+'.csv'
+#           dual_norm = np.loadtxt(dirs+fname)
+#           data = np.array(dual_norm).astype(np.float64)
+            filenames = reader.find_files(fname, '../')
+#           print(['Ra'+str(int(P_train[minidx])), model[minidx], str(N[minidx])])
+            for f in filenames:
+                if all(x in f for x in ['Ra'+str(int(P_train[minidx])), model[minidx], str(N[minidx])]):
+#               if 'Ra'+str(int(P_train[minidx])) in re.split('[/_]', f) and model[minidx] in re.split('[/_]', f):
+#               if 'Re'+str(int(P_train[minidx])) in re.split('[/_]', f):
+                    filename = f
+            data = pd.read_csv(filename)
+            erri.append(data['dual_norm'][i])
         idx = erri.index(min(erri))
         P_test_anchor.append(near_anch[idx])
     return P_test_anchor
@@ -108,21 +120,29 @@ def get_opterri(P_test, P_train, N, P_test_anchor, ax, colors, model, mode,
 def get_opterri_param(P_test, P_train, N, P_test_anchor, ax, colors, model, mode, fd,
                 scaled=''):
     import numpy as np
+    import pandas as pd
+    import reader
+    import re
     # get erri with theta_g at each anchor points
     erri_his = []
     for i, train in enumerate(P_train):
-        dirs = '../ra_'+str(train)+'_theta_90/'+model[i]+'_parameter_ra_'+str(train) + \
-               '/dual_norm/'
-        filename = 'param_list_'+mode+'.dat'
-        angle = np.loadtxt(dirs+filename)
         if model[i] == 'l-rom':
-            filename = 'erri_N'+str(N[i])+'_'+fd[i].strip('0')+scaled+'_'+mode+'.dat'
+            fname = 'dual_norm_N'+str(N[i])+'_'+fd[i].strip('0')+scaled+'_'+mode+'.dat'
+        elif model[i] == 'l-rom-df':
+            fname = 'dual_norm_N'+str(N[i])+'_0'+fd[i]+scaled+'.csv'
         else:
-            filename = 'erri_N'+str(N[i])+scaled+'_'+mode+'.dat'
-        erri = np.loadtxt(dirs+filename)
+            fname = 'dual_norm_N'+str(N[i])+scaled+'.csv'
+        filenames = reader.find_files(fname, '../')
+        for f in filenames:
+            if all(x in f for x in ['Ra'+str(train), model[i], str(N[i])]):
+#           if 'Ra'+str(train) in re.split('[/_]', f):
+#           if 'Re'+str(train) in re.split('[/_]', f):
+                filename = f
+                print(filename)
+        data = pd.read_csv(filename)
 
-        erri_his.append(erri)
-        ax.semilogy(angle, erri, '--o', color=colors[i], mfc="None",
+        erri_his.append(data.iloc[:, 1])
+        ax.semilogy(data.iloc[:, 0], data.iloc[:, 1], '--o', color=colors[i], mfc="None",
                     label=r'\textit{it} = '+str(i+1))
 
     erri_comb = np.array(erri_his)
@@ -155,49 +175,95 @@ def get_anchor_qoi(i, train, N, model, mode):
 
 def get_anchor_qoi_param(i, train, N, model, mode, fd):
     import numpy as np
+    import pandas as pd
+    import re
+    import reader
 
     dirs = '../ra_'+str(train)+'_theta_90/'+model[i]+'_parameter_ra_'+str(train)+'/nu/'
     if model[i] == 'l-rom':
-        filename = 'param_'+mode+'.dat'
-        perc = fd[i].strip('0')
-        angle = np.loadtxt(dirs+filename)
-        filename = 'merr_N'+str(N[i])+'_'+perc+'_'+mode+'.dat'
-        merr = np.loadtxt(dirs+filename)
-        filename = 'stderr_N'+str(N[i])+'_'+perc+'_'+mode+'.dat'
-        sderr = np.loadtxt(dirs+filename)
-        filename = 'mnu_N'+str(N[i])+'_'+perc+'_'+mode+'.dat'
-        m = np.loadtxt(dirs+filename)
-        filename = 'stdnu_N'+str(N[i])+'_'+perc+'_'+mode+'.dat'
-        sd = np.loadtxt(dirs+filename)
+        fname = 'mnurelerr_N'+str(N[i])+'_'+fd[i].strip('0')+'_'+mode+'.csv'
+    elif model[i] == 'l-rom-df':
+        fname = 'mnurelerr_N'+str(N[i])+'_0'+fd[i]+'.csv'
+        filenames = reader.find_files(fname, '../')
+        for f in filenames:
+#           if 'Ra'+str(train) in re.split('[/_]', f):
+            if all(x in f for x in ['Ra'+str(train), model[i], str(N[i])]):
+                filename = f
+        data = pd.read_csv(filename)
+        param = data['Ra']
+        merr = data['mnurelerr']
+
+        fname = 'mnu_N'+str(N[i])+'_0'+fd[i]+'.csv'
+        filenames = reader.find_files(fname, '../')
+        for f in filenames:
+#           if 'Ra'+str(train) in re.split('[/_]', f):
+            if all(x in f for x in ['Ra'+str(train), model[i], str(N[i])]):
+                filename = f
+        data = pd.read_csv(filename)
+        m = data['mnu']
+        
+        fname = 'std_nu_N'+str(N[i])+'_0'+fd[i]+'.csv'
+        filenames = reader.find_files(fname, '../')
+        for f in filenames:
+#           if 'Ra'+str(train) in re.split('[/_]', f):
+            if all(x in f for x in ['Ra'+str(train), model[i], str(N[i])]):
+                filename = f
+        data = pd.read_csv(filename)
+        std = data['std_nu']
     else:
-        filename = 'param_'+mode+'.dat'
-        angle = np.loadtxt(dirs+filename)
-        filename = 'merr_N'+str(N[i])+'_'+mode+'.dat'
-        merr = np.loadtxt(dirs+filename)
-        filename = 'stderr_N'+str(N[i])+'_'+mode+'.dat'
-        sderr = np.loadtxt(dirs+filename)
-        filename = 'mnu_N'+str(N[i])+'_'+mode+'.dat'
-        m = np.loadtxt(dirs+filename)
-        filename = 'stdnu_N'+str(N[i])+'_'+mode+'.dat'
-        sd = np.loadtxt(dirs+filename)
+#       filename = 'param_'+mode+'.dat'
+#       angle = np.loadtxt(dirs+filename)
+#       filename = 'merr_N'+str(N[i])+'_'+mode+'.dat'
+#       merr = np.loadtxt(dirs+filename)
+#       filename = 'stderr_N'+str(N[i])+'_'+mode+'.dat'
+#       sderr = np.loadtxt(dirs+filename)
+#       filename = 'mnu_N'+str(N[i])+'_'+mode+'.dat'
+#       m = np.loadtxt(dirs+filename)
+#       filename = 'stdnu_N'+str(N[i])+'_'+mode+'.dat'
+#       sd = np.loadtxt(dirs+filename)
+        fname = 'mnurelerr_N'+str(N[i])+'.csv'
+        filenames = reader.find_files(fname, '../')
+        for f in filenames:
+#           if 'Ra'+str(train) in re.split('[/_]', f):
+            if all(x in f for x in ['Ra'+str(train), model[i], str(N[i])]):
+                filename = f
+        data = pd.read_csv(filename)
+        param = data['Ra']
+        merr = data['mnurelerr']
 
-    return angle, merr, sderr, m, sd
+        fname = 'mnu_N'+str(N[i])+'.csv'
+        filenames = reader.find_files(fname, '../')
+        for f in filenames:
+#           if 'Ra'+str(train) in re.split('[/_]', f):
+            if all(x in f for x in ['Ra'+str(train), model[i], str(N[i])]):
+                filename = f
+        data = pd.read_csv(filename)
+        m = data['mnu']
+        
+        fname = 'std_nu_N'+str(N[i])+'.csv'
+        filenames = reader.find_files(fname, '../')
+        for f in filenames:
+#           if 'Ra'+str(train) in re.split('[/_]', f):
+            if all(x in f for x in ['Ra'+str(train), model[i], str(N[i])]):
+                filename = f
+        data = pd.read_csv(filename)
+        std = data['std_nu']
+
+    return param, merr, m, std
 
 
-def get_optqoi(merr_all, m_all, sderr_all, sd_all, P_test, P_train,
+def get_optqoi(merr_all, m_all, sd_all, P_test, P_train,
                P_test_anchor, model, scaled=''):
     opt_merr_nu = []
-    opt_stderr_nu = []
     opt_m_nu = []
     opt_std_nu = []
     P_train = list(P_train)
     for i, test in enumerate(P_test):
         index = P_train.index(P_test_anchor[i])
         opt_merr_nu.append(merr_all[index, i])
-        opt_stderr_nu.append(sderr_all[index, i])
         opt_m_nu.append(m_all[index, i])
         opt_std_nu.append(sd_all[index, i])
-    return opt_merr_nu, opt_stderr_nu, opt_m_nu, opt_std_nu
+    return opt_merr_nu, opt_m_nu, opt_std_nu
 
 
 def get_anchor_flderr(i, train, N, model, mode):
@@ -218,8 +284,12 @@ def get_anchor_flderr(i, train, N, model, mode):
 def get_anchor_flderr_param(i, train, N, model, mode, fd):
     # return both the projection and rom fld relative errro
     import numpy as np
+    import pandas as pd
+    import reader
+    import re
 
     dirs = '../ra_'+str(train)+'_theta_90/'+model[i]+'_parameter_ra_'+str(train)+'/mrelerr/'
+    dirs = '../re'+str(train)+'/'+model[i]+'_parameter_Re'+str(train)+'/mrelerr_h1/'
     if model[i] == 'l-rom':
         filename = 'param_list_'+mode+'.dat'
         angle = np.loadtxt(dirs+filename)
@@ -227,15 +297,20 @@ def get_anchor_flderr_param(i, train, N, model, mode, fd):
         fldrelerr_rom = np.loadtxt(dirs+filename)
         filename = 'proj_relerr_N'+str(N[i])+'_'+mode+'.dat'
         fldrelerr_proj = np.loadtxt(dirs+filename)
+    elif model[i] == 'l-rom-df':
+        fname = 'mrelerr_N'+str(N[i])+'_0'+fd[i]+'.csv'
     else:
-        filename = 'param_list_'+mode+'.dat'
-        angle = np.loadtxt(dirs+filename)
-        filename = 'rom_relerr_N'+str(N[i])+'_'+mode+'.dat'
-        fldrelerr_rom = np.loadtxt(dirs+filename)
-        filename = 'proj_relerr_N'+str(N[i])+'_'+mode+'.dat'
-        fldrelerr_proj = np.loadtxt(dirs+filename)
+        fname = 'mrelerr_N'+str(N[i])+'.csv'
 
-    return angle, fldrelerr_rom, fldrelerr_proj
+    filenames = reader.find_files(fname, '../')
+    for f in filenames:
+        if all(x in f for x in ['Ra'+str(train), model[i], str(N[i])]):
+#       if 'Ra'+str(train) in re.split('[/_]', f):
+#       if 'Re'+str(train) in re.split('[/_]', f):
+            filename = f
+    data = pd.read_csv(filename)
+
+    return [data[i].to_numpy() for i in data.columns]
 
 
 def get_optflderr(flderr_rom, flderr_proj, P_test, P_train,
--- code/plot_helpers/reader.py
+++ code/plot_helpers/reader.py
@@ -1,3 +1,4 @@
+import re
 def reader(fname):
     with open(fname, 'r') as f:
         k = f.read()
@@ -45,7 +46,15 @@ def erri_w_param(model, anchor, N, mode, *argv):
         elif str(argv[0]) == 'eta_rom':
             fname = 'erri_N'+N+'_sc_eta_rom_'+mode+'.dat'
 
-    filename = '../ra_'+str(anchor)+'_theta_90/'+model+'_parameter_theta90_Ra'+str(anchor)+'/dual_norm/'+fname
+#   filename = '../ra_'+str(anchor)+'_theta_90/'+model+'_parameter_theta90_Ra'+str(anchor)+'/dual_norm/'+fname
+#   filename = '../re'+str(anchor)+'/'+model+'_parameter_Re'+str(anchor)+'/dual_norm/'+fname
+    filenames = find_files(fname, '../')
+    for f in filenames:
+#       if 'Re'+str(anchor) in re.split('[/_]', f):
+        if all(x in f for x in ['Ra'+str(anchor), model, str(N)]):
+#       if 'Ra'+str(anchor) in re.split('[/_]', f):
+            print(f)
+            filename = f
     data = pd.read_csv(filename)
 
     return [data[i].to_numpy() for i in data.columns]
@@ -54,7 +63,7 @@ def erri_w_param(model, anchor, N, mode, *argv):
 def erri_leray_w_param(model, anchor, N, mode, pt, *argv):
     import pandas as pd
     if len(argv) == 0:
-        fname = 'dual_norm_N'+str(N)+'_'+pt+'.csv'
+        fname = 'dual_norm_N'+str(N)+'_0'+pt+'.csv'
     else:
         if str(argv[0]) == 'fom':
             fname = 'erri_N'+N+'_sc_fom_'+mode+'.dat'
@@ -65,7 +74,15 @@ def erri_leray_w_param(model, anchor, N, mode, pt, *argv):
         elif str(argv[0]) == 'eta_rom':
             fname = 'erri_N'+N+'_sc_eta_rom_'+mode+'.dat'
 
-    filename = '../ra_'+str(anchor)+'_theta_90/'+model+'_parameter_theta90_Ra'+str(anchor)+'/dual_norm/'+fname
+#   filename = '../ra_'+str(anchor)+'_theta_90/'+model+'_parameter_theta90_Ra'+str(anchor)+'/dual_norm/'+fname
+    print(fname)
+    filenames = find_files(fname, '../')
+    for f in filenames:
+#       if 'Re'+str(anchor) in re.split('[/_]', f):
+#       if 'Ra'+str(anchor) in re.split('[/_]', f):
+        if all(x in f for x in ['Ra'+str(anchor), model, str(N)]):
+            print(f)
+            filename = f
     data = pd.read_csv(filename)
 #   filename = '../ra_'+str(anchor)+'_theta_90/'+model+'_parameter_ra_'+str(anchor)+'/dual_norm/param_list_'+mode+'.dat'
 #   angle = np.loadtxt(filename)
@@ -74,3 +91,14 @@ def erri_leray_w_param(model, anchor, N, mode, pt, *argv):
 #   erri = np.loadtxt(filename)
 
     return [data[i].to_numpy() for i in data.columns]
+
+
+def find_files(filename, search_path):
+    import os
+    result = []
+
+# Wlaking top-down from the root
+    for root, dir, files in os.walk(search_path):
+        if filename in files:
+            result.append(os.path.join(root, filename))
+    return result
