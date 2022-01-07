4dfc6737c2c9bfc1b413424c42456a89c5333bf8
Ping-Hsuan <pht2@illinois.edu>
2022-01-07 12:35:46 -0600

    update pattern for greping

--- postprocessing/aux/gptr.py
+++ postprocessing/aux/gptr.py
@@ -10,10 +10,12 @@ def gptr(model, N=None, T0=None, mode=None, fd=None):
         ptr = ptr+'_zero_h10_'
     if fd is not None:
         print(fd, model)
-        if model != 'l-rom' and model != 'l-rom_df':
-            raise Exception("fd only supported for l-rom or l-rom_df")
+        if model != 'l-rom' and model != 'l-rom-df':
+            raise Exception("fd only supported for l-rom or l-rom-df")
         else:
-            ptr = ptr+'(-?\d+)_(-?\d+)_'+fd+'_.*'
+#           ptr = ptr+'re(-?\d+)_'+fd+'_.*'
+            ptr = ptr+'theta(-?\d+)_ra(-?\d+)_'+fd+'_.*'
     else:
-        ptr = ptr+'(-?\d+)_(-?\d+)_.*'
+#       ptr = ptr+'re(-?\d+)_.*'
+        ptr = ptr+'theta(-?\d+)_ra(-?\d+)_.*'
     return ptr
