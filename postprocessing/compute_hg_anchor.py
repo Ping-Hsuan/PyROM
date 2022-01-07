9214569edc293357425568cda2e5f47eebad1fdb
Sean <r04246004@ntu.edu.tw>
2022-01-07 12:45:07 -0600

    update compute hg anchor script

--- postprocessing/compute_hg_anchor.py
+++ postprocessing/compute_hg_anchor.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 import sys
 import os
 sys.path.append('/Users/bigticket0501/Developer/PyMOR/code/plot_helpers/')
-import setup
 from reader import erri_w_theta
 from reader import erri_w_param
 from reader import erri_leray_w_param
+from figsetup.style import style
+from figsetup.color import color
+from figsetup.text import text
 import checker
 import mypostpro
 import re
@@ -14,10 +16,12 @@ import collections
 import csv
 import yaml
 import math
+from hg_offline.helper import get_anchor_mabserr
+from hg_offline.helper import get_anchor_mrelerr
 
-setup.style(1)
-colors = setup.color(0)
-setup.text()
+style(1)
+colors = color(0)
+text()
 
 print("---------------------------------------------")
 print("This is the name of the program:", sys.argv[0])
@@ -27,10 +31,10 @@ mode = str(sys.argv[2])
 print("---------------------------------------------")
 
 if len(sys.argv) <= 3:
-    ylb = r'$\triangle$'
+    ylb = r'$\Delta$'
 #   ylb = 'Error indicator'
     tag = ''
-    title = r'POD-$h$Greedy, offline stage:'+'\n Error indicator'
+    title = r'POD-$h$Greedy, offline stage: Error indicator'
 else:
     if sys.argv[3] == 'fom':
 #       ylb = r'$\frac{\triangle(\theta_g)}{\|u_{FOM}(\theta^*_g)\|_{H^1}}$'
@@ -66,17 +70,11 @@ else:
 with open('train_info.yaml') as f:
     features = yaml.load(f, Loader=yaml.FullLoader)
 
+print("POD-hGreedy information:")
 data = []
 for i, key in enumerate(features.keys()):
+    print(key+": ", features[key])
     data.append(features[key])
-
-
-print("POD-hGreedy information:")
-print("Iteration: ", data[0])
-print("Anchor points: ", data[1])
-print("N: ", data[2])
-print("K: ", data[3])
-print("model: ", data[6])
 print("---------------------------------------------")
 
 color_ctr = 0
@@ -88,26 +86,46 @@ erri_his = []
 erri_anchor = []
 max_erri = []
 
-
 for itr, anchor, N, K, model in zip(data[0], data[1], data[2], data[3], data[6]):
-
 
     print(itr, anchor, N, K, model)
 
     if len(sys.argv) <= 3:
 #       angle, erri = erri_w_theta(model, anchor, N, mode)
-        if model != 'l-rom':
+        if model != 'l-rom' and model !='l-rom-df':
             angle, erri = erri_w_param(model, anchor, N, mode)
         else:
             angle, erri = erri_leray_w_param(model, anchor, N, mode, data[7][itr-1])
+        if features['scaled'] == 'mabserr':
+            param, mabserr_rom, mabserr_proj = get_anchor_mabserr(anchor, N, model, mode, data[7][itr-1])
+            idx = np.where(param == anchor)
+            erri = erri/mabserr_rom[idx]
+            ylb = r'$\Delta$'
+            title = 'POD-$h$Greedy, offline stage: \n'+r'Error indicator'+' at '+r'$\mathcal{P}_{train}$'+' scaled with absolute $H^1$ error'
+        elif features['scaled'] == 'mrelerr':
+            param, mrelerr_rom, mrelerr_proj = get_anchor_mrelerr(anchor, N, model, mode, data[7][itr-1])
+            idx = np.where(param == anchor)
+            erri = erri/mrelerr_rom[idx]
+            ylb = r'$\Delta$'
+            title = 'POD-$h$Greedy, offline stage: \n'+r'Error indicator'+' at '+r'$\mathcal{P}_{train}$'+' scaled with relative $H^1$ error'
+        elif features['scaled'] == 'eff':
+            param, mabserr_rom, mabserr_proj = get_anchor_mabserr(anchor, N, model, mode, data[7][itr-1])
+            idx = np.where(param == anchor)
+            scale = erri[idx]/mabserr_rom[idx]
+            erri = erri/scale
+            ylb = r'$\Delta$'
+            title = r'POD-$h$Greedy, offline stage: Error indicator'+' at '+r'$\mathcal{P}_{train}$'+' scaled with effectivity'
+        elif features['scaled'] is None:
+            features['scaled'] = 'nosc'
     else:
 #       angle, erri = erri_w_theta(model, anchor, N, mode, sys.argv[3])
         if model != 'l-rom':
             angle, erri = erri_w_param(model, anchor, N, mode, sys.argv[3])
         else:
             angle, erri = erri_leray_w_param(model, anchor, N, mode, data[7][itr-1], sys.argv[3])
-
-    ax.semilogy(angle, erri, '--o', color=colors[itr-1], mfc="None", label=r'$iter = $ '+str(itr))
+    print(angle, erri)
+    ax.semilogy(angle, erri, '--o', color=colors[itr-1], mfc="None", label=r'$\mathsf{Iter} = $ '+str(itr))
+    print(anchor)
     idx = np.where(angle == int(anchor))
     erri_anchor.append(erri[idx])
     anchors.append(int(anchor))
@@ -128,11 +146,11 @@ for itr, anchor, N, K, model in zip(data[0], data[1], data[2], data[3], data[6])
     else:
         ax.set_yscale('linear')
         ylim_exp = math.ceil(math.log10(min(erri)))-1
-        ylim = [min(erri)-10**ylim_exp, None]
+        ylim = [float(i) for i in features['yrange']]
 
     ax.set(xlabel=r'$'+features['Param']+'$', ylabel=ylb,
            ylim=ylim, xticks=features['Ptrain'])
-    ax.set_title(title+' at '+r'$\mathcal{P}_{train}$')
+    ax.set_title(title)
 
     candidate = str(int(angle[idxmax]))
     while candidate in data[1][:itr]:
@@ -147,17 +165,17 @@ for itr, anchor, N, K, model in zip(data[0], data[1], data[2], data[3], data[6])
     ax.legend(loc=0, ncol=1)
     ax.axes.grid(True, axis='y')
     ax.set_xticklabels(ax.get_xticks(), rotation=45)
-    fig.savefig('offline_erri_L'+str(itr)+tag+'.png')
+    fig.savefig('offline_erri_L'+str(itr)+'_'+features['scaled']+'.png')
     l = anc.pop(0)
     l.remove()
     lmin = lmin.pop(0)
     lmin.remove()
 
 fig, ax = plt.subplots(1, tight_layout=True)
-ax.set(xlabel='Iteration', ylabel=r'$\max{\triangle('+features['Param']+')}$',
+ax.set(xlabel='Iteration', ylabel=r'$\max{\Delta('+features['Param']+')}$',
        xlim=[0, max(data[0])+1], ylim=[1e-3, 1e0])
 ax.semilogy(data[0], max_erri, 'k-o')
 
 np.savetxt('iter'+tag+'.dat', data[0])
-np.savetxt('max_erri'+tag+'.dat', max_erri)
-fig.savefig('max_erri_w_iter'+tag+'.png')
+np.savetxt('max_erri_'+features['scaled']+'.dat', max_erri)
+fig.savefig('max_erri_w_iter_'+features['scaled']+'.png')
